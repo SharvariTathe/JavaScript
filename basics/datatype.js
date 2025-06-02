90128af7af7453ec131e048106ed0eee79d13622
SharvariTathe <sharvarit05@gmail.com>
2025-06-02 19:55:34 +0530

    Create datatype.js

---
+++ basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict"  // treat all js code as newer version
+
+// alert(3+3) we are using vs not , browser --it shows a pop up with 6
+console.log(3+3)
+
+let name="sharvari"
+let age= 19
+let isLoggedIn= false 
+
+// primitive -->
+// number range 2 to power 53
+// bigint for very big no.s
+// string in ""
+// boolean - true or false
+// null standalone value - it has no value it is empty
+// undefined - no value given yet
+// symbol - to find uniqueness
+
+//object
+console.log(typeof "sharvari") // gives string
+console.log(typeof null) // gives object
